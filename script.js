6bf543b273567a02bfa0116f077c8bc84ad94c61
pealungthun628-cloud <pealungthun628@gmail.com>
2026-07-15 13:52:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function showMessage() {
+    const msg = document.getElementById("topMessage");
 
+    msg.style.top = "20px";
+
+    setTimeout(() => {
+        msg.style.top = "-60px";
+    }, 3000);
+}
